03ba40a76bb702c46c92ba46804f1bef79eb0441
SotaTek-TungNguyen2 <tung.nguyen2@sotatek.com>
2022-03-06 17:05:21 +0700

    init

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,22 @@
+import {Block, Transaction} from '../types';
+
+export async function handleBlock(block: any): Promise<void> {
+  let record = await Block.get(block.block.blockhash);
+  if (!record) {
+      record = new Block(block.block.blockhash);
+  }
+  //Record block number
+  record.slot = block.block.parentSlot + 1;
+  await record.save();
+}
+
+export async function handleTransaction(transaction: any): Promise<void> {
+  let record = new Transaction(transaction.transaction.message.recentBlockhash);
+  record.blockHash = transaction.transaction.message.recentBlockhash;
+  record.slot = transaction.slot;
+  record.blockHeight = transaction.blockHeight;
+  record.signature = transaction.transaction.signatures[0];
+  record.programId = [...new Set(transaction.meta.logMessages.map(log => log.split(' ')[1]))] as string[];
+  record.status = Object.keys(transaction.meta.status)[0];
+  await record.save();
+}
